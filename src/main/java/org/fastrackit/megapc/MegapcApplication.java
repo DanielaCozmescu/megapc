19ed2b33a287847cc3c31b9b10e5881025cfa3ba
Daniela Cozmescu <dani_21bcn@hotmail.com>
2020-07-11 10:45:03 +0300

    Initial commit (project structure)

---
+++ src/main/java/org/fastrackit/megapc/MegapcApplication.java
@@ -0,0 +1,13 @@
+package org.fastrackit.megapc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegapcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegapcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fastrackit/megapc/MegapcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fastrackit.megapc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegapcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
